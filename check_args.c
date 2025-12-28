21b5ff33311422693fe25e579dd6f39e183de283
ChihabAbourkia <abourkiac@gmail.com>
2025-12-28 17:06:26 +0100

    split and atot and check_number

--- check_args.c
+++ check_args.c
@@ -11,8 +11,51 @@
 /* ************************************************************************** */
 
 #include"push_swap.h"
+#include<stdio.h>
 void error(void)
 {
 	write(2, "Error\n", 6);
 	exit(1);
+}
+int check_int(char *str)
+{
+	int i = 0;
+	if(!str[i])
+		error();
+	if((str[i] == '-' ||str[i] == '+')&&(str[i+1] >= '0' && str[i+1] <= '9'))
+		i++;
+	while(str[i])
+	{
+		if(!(str[i] >= '0' && str[i] <= '9'))
+		{
+			error();
+		}
+		i++;
+	}
+	return 1;
+
+}
+long ft_atol(char *str)
+{
+	int i = 0;
+	int s = 1;
+	long r = 0;
+	while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if(str[i] == '-' ||str[i] == '+')
+	{
+		if(str[i] == '-')
+			s = -1;;
+		i++;
+	}
+	while(str[i] >= '0' && str[i] <= '9')
+	{
+		r = r * 10 + (str[i] - '0');
+		i++;
+	}
+	long results = r * s;
+	if(!(results >= -2147483648 && results <= 2147483647))
+		error();
+	return results;
+
 }
---
+++ split.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chabourk <chabourk@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/25 18:00:30 by chabourk          #+#    #+#             */
+/*   Updated: 2025/12/26 19:37:51 by chabourk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	count_words(char *str, char sep)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] != sep && (str[i + 1] == sep || str[i + 1] == '\0'))
+		{
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
+char	**free_splited(char **splited, int j)
+{
+	int	i;
+
+	i = 0;
+	while (i < j)
+	{
+		free(splited[i]);
+		i++;
+	}
+	free(splited);
+	return (NULL);
+}
+void	split_copy(char **splited, char *str, int start, int end, int j)
+{
+	int	n;
+
+	n = 0;
+	while (start < end)
+	{
+		splited[j][n++] = str[start++];
+	}
+	splited[j][n] = '\0';
+}
+char	**splited_fill(char **splited, char *str, char sep)
+{
+	int	i;
+	int	j;
+	int	start;
+	int	end;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		while (str[i] == sep && str[i] != '\0')
+			i++;
+		start = i;
+		while (str[i] != sep && str[i])
+			i++;
+		end = i;
+		splited[j] = malloc((end - start) + 1);
+		if (!splited[j])
+			free_splited(splited, j);
+		split_copy(splited, str, start, end, j);
+		j++;
+	}
+	splited[j] = NULL;
+	return (splited);
+}
+char	**split(char *str, char sep)
+{
+	int		words_count;
+	char	**splited;
+	char	**splited_str;
+
+	words_count = count_words(str, sep);
+	splited = malloc((words_count + 1) * sizeof(char *));
+	if (!splited)
+	{
+		return (NULL);
+	}
+	splited_str = splited_fill(splited, str, sep);
+	if (!splited_str)
+		return (NULL);
+	return (splited_str);
+}
+int	main(void)
+{
+	char	**str;
+	int		i;
+
+	str = split(" chi choo bii", ' ');
+	i = 0;
+	while (str[i])
+	{
+		printf("%s\n", str[i]);
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
